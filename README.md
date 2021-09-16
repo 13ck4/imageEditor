cd02f7a97655408de20227e01e58b20c9a2e96bf
luat <nguyen-ngoc-luat@datanext.co.jp>
2021-09-16 16:20:56 +0700

    image Editor

---
+++ README.md
@@ -0,0 +1,49 @@
+# Wath is this?
+
+
+Get perfect shadow every time for the non-designer.
+
+# Installation 
+
+`npm i imageEditor --save`
+
+Then... 
+
+```
+var imageEditor = require('imageEditor')
+
+------
+step1: ***** load library javascript **** 
+            imageEditor.createEditor({load: true})
+-----
+
+-----
+step2: create editor image
+
+    npmimageEditor.createEditor({
+        id: "tui-image-editor-container",
+        srcImage: srcImage, 
+        locale_Ja: locale_Ja,
+        initMenu: initMenu, 
+        menuBarPosition: menuBarPosition, 
+        menu: menu, 
+        width: width, 
+        height: height
+    })
+
+```
+
+## Options
+
+borderNPM supports 9 options, both of which are optional:
+* *load*                  -_boolean
+
+
+* *id*                  -_string_   (id of div)
+* *srcImage*            -_string_   (url of image)
+* *locale_Ja*           _array_     (language of taskbar , Defaults to english)
+* *srcImage*            -_string_   (url of image)
+* *initMenu*            -_string_   (Defaults to filter)
+* *menuBarPosition*     -_tring_    (Defaults to bottom)
+* *width*               -_integer_  (width of image, default 700px)
+* *height*              -_integer_  (height of image, default 500px)
---
+++ index.js
@@ -0,0 +1,64 @@
+function includeCss(cssFilePath) {
+    var css = document.createElement("link");
+    css.rel = "stylesheet";
+    css.type = "text/css";
+    css.href = cssFilePath;
+    document.head.appendChild(css);
+}
+function includeJs(jsFilePath) {
+    var js = document.createElement("script");
+    js.type = "text/javascript";
+    js.src = jsFilePath;
+    document.head.appendChild(js);
+}
+function createEditor(options){
+    if(options.load){
+        includeCss("https://cdn.jsdelivr.net/npm/tui-color-picker@2.2.7/dist/tui-color-picker.css");
+        includeCss("https://cdnjs.cloudflare.com/ajax/libs/tui-image-editor/3.15.0/tui-image-editor.css");
+        includeJs("https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.3.1/fabric.min.js");
+        includeJs("https://cdnjs.cloudflare.com/ajax/libs/tui-code-snippet/1.5.2/tui-code-snippet.min.js");
+        includeJs("https://cdn.jsdelivr.net/npm/tui-color-picker@2.2.7/dist/tui-color-picker.min.js");
+        setTimeout(() => {
+            includeJs("https://cdnjs.cloudflare.com/ajax/libs/tui-image-editor/3.12.0/tui-image-editor.min.js");
+        }, 100);
+        
+    }else{
+        const myTheme = {
+            "header.display": "none",
+        };
+
+        var initMenu = 'filter';
+        var menuBarPosition =  'bottom';
+        var width = 700;
+        var height = 500;
+        if(options.initMenu){
+            initMenu = options.initMenu
+        }
+        if(options.menuBarPosition){
+            menuBarPosition = options.menuBarPosition
+        }
+        if(options.width){
+            width = options.width
+        }
+        if(options.height){
+            height = options.height
+        }
+        new tui.ImageEditor(document.getElementById(options.id), {
+            includeUI: {
+                loadImage: {
+                    path: options.srcImage,
+                    name: 'SampleImage',
+                },
+                locale: options.locale_Ja,
+                theme: myTheme,
+                initMenu: initMenu,
+                menuBarPosition: menuBarPosition,
+                menu: options.menu,
+            },
+            cssMaxWidth: width,
+            cssMaxHeight: height,
+            usageStatistics: false,
+        });
+    }
+}
+module.exports.createEditor = createEditor;
